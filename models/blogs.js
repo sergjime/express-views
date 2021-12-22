9f11e4f2d047101ed0d041b74496f948e2e86c28
Sergio <sergjimedaw10@gmail.com>
2021-12-22 15:00:32 +0100

    modelo de blogs

---
+++ models/blogs.js
@@ -0,0 +1,20 @@
+module.exports = [
+  {
+    id: 1,
+    title: "Primer Blog",
+    resume: "Resumen del primer blog",
+    body: "dfgsdfgñiijasdkfj ",
+  },
+  {
+    id: 2,
+    title: "Segundo Blog",
+    resume: "Resumen del segundo blog",
+    body: "dfgsdfgñiijasdkfj ",
+  },
+  {
+    id: 3,
+    title: "Tercer Blog",
+    resume: "Resumen del tercero blog",
+    body: "dfgsdfgñiijasdkfj ",
+  },
+];
