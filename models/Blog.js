fa259b312cbd88dc5d9326a2ef71e17134a57caf
Sergio <sergjimedaw10@gmail.com>
2022-01-10 14:56:08 +0100

    modelo de blogs

---
+++ models/Blog.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const blogSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    resume: {
+      type: String,
+      required: true,
+    },
+    body: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Blog = mongoose.model("blog", blogSchema);
+
+module.exports = Blog;
