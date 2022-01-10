e4689c6c996e86266a84e9b492981af489a54539
Sergio <sergjimedaw10@gmail.com>
2022-01-10 14:57:08 +0100

    rutas de blogs

--- routes/blogsRoutes.js
+++ routes/blogsRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const blogs = require("../models/blogs");
+const blogs = require("../models/Blog");
 const blogController = require("../controllers/blogsController");
 const router = express.Router();
 
@@ -10,6 +10,10 @@ router.get("/", (req, res) => {
 router.post("/", blogController.blog_create_post);
 
 router.get("/create", blogController.blog_create_get);
+
+router.get("/update/:id", blogController.blog_update_get);
+
+router.post("/update/:id", blogController.blog_update_post);
 
 router.get("/:id", blogController.blog_details);
 
